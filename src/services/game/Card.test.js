58e6a3a5bbccf2c217a96985452d6ec1fc5dad59
anti-matt-er <m.burrows@live.co.uk>
2021-01-03 16:33:59 +0000

    test to make sure valid cards are accepted

--- src/services/game/Card.test.js
+++ src/services/game/Card.test.js
@@ -6,10 +6,16 @@ it('should allow only valid suits', () => {
     expect(() => {
         new Card('A', 'X'); //an ace of invalid suit "X"
     }).toThrow('valid suit');
+    expect(() => {
+        new Card('A', 'S'); //a valid ace of spades
+    }).not.toThrow();
 });
 
 it('should allow only valid ranks', () => {
     expect(() => {
         new Card('X', 'S'); //an invalid rank "X" of spades
     }).toThrow('valid rank');
+    expect(() => {
+        new Card('A', 'S'); //a valid ace of spades
+    }).not.toThrow();
 });
