062d80f29cb20af6b09ab9f37737dca9c5ac3ddc
anti-matt-er <m.burrows@live.co.uk>
2021-01-03 16:29:14 +0000

    implement invalid rank/suit check

--- src/services/game/Card.js
+++ src/services/game/Card.js
@@ -2,8 +2,32 @@
 
 class Card {
     constructor(rank, suit) {
+        this.valid_ranks = [
+            'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'
+        ];
+        this.valid_suits = [
+            'C', 'S', 'H', 'D'
+        ];
         this.rank = rank;
         this.suit = suit;
+        this.validate();
+    }
+
+    validate() {
+        this.checkRank();
+        this.checkSuit();
+    }
+
+    checkRank() {
+        if (!this.valid_ranks.includes(this.rank)) {
+            throw 'Error constructing card: "' + this.rank + '" is an invalid rank!'
+        }
+    }
+
+    checkSuit() {
+        if (!this.valid_suits.includes(this.suit)) {
+            throw 'Error constructing card: "' + this.suit + '" is an invalid suit!'
+        }
     }
 
     get value() {
