400221226c0977e8501397a312a505036b8b1b31
anti-matt-er <m.burrows@live.co.uk>
2021-01-03 16:43:28 +0000

    test "10" is an accepted rank

--- src/services/game/Card.test.js
+++ src/services/game/Card.test.js
@@ -6,6 +6,7 @@ it('should allow only valid suits', () => {
     expect(() => {
         new Card('A', 'X'); //an ace of invalid suit "X"
     }).toThrow('valid suit');
+
     expect(() => {
         new Card('A', 'S'); //a valid ace of spades
     }).not.toThrow();
@@ -15,7 +16,18 @@ it('should allow only valid ranks', () => {
     expect(() => {
         new Card('X', 'S'); //an invalid rank "X" of spades
     }).toThrow('valid rank');
+
     expect(() => {
         new Card('A', 'S'); //a valid ace of spades
     }).not.toThrow();
+});
+
+it('should allow "10" as a valid rank, converting to "T"', () => {
+    var card_TD;
+
+    expect(() => {
+        card_TD = new Card('10', 'D'); //a ten of diamonds, with undesired notation
+    }).not.toThrow();
+    
+    expect(card_TD.rank).toEqual('T');
 });
