6b10553548731d62886ebcea24668939062619c6
anti-matt-er <m.burrows@live.co.uk>
2021-01-03 16:12:20 +0000

    initial test for Card

---
+++ src/services/game/Card.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import { Card } from './Card';
+
+it('should allow only valid suits', () => {
+    expect(() => {
+        new Card('A', 'X'); //an ace of invalid suit "X"
+    }).toThrow('valid suit');
+});
+
+it('should allow only valid ranks', () => {
+    expect(() => {
+        new Card('X', 'S'); //an invalid rank "X" of spades
+    }).toThrow('valid rank');
+});
