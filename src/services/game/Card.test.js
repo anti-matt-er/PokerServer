0f7bdc1fe90caa64c681cb10fe717cd65b951bcb
anti-matt-er <m.burrows@live.co.uk>
2021-01-03 16:54:30 +0000

    test card values

--- src/services/game/Card.test.js
+++ src/services/game/Card.test.js
@@ -28,6 +28,20 @@ it('should allow "10" as a valid rank, converting to "T"', () => {
     expect(() => {
         card_TD = new Card('10', 'D'); //a ten of diamonds, with undesired notation
     }).not.toThrow();
-    
+
     expect(card_TD.rank).toEqual('T');
+});
+
+it('should return correct values for cards', () => {
+    var card_2C = new Card('2', 'C');
+    var card_4D = new Card('4', 'D');
+    var card_TD = new Card('T', 'D');
+    var card_KH = new Card('K', 'H');
+    var card_AS = new Card('A', 'S');
+
+    expect(card_2C.value).toEqual(1);
+    expect(card_4D.value).toEqual(3);
+    expect(card_TD.value).toEqual(9);
+    expect(card_KH.value).toEqual(12);
+    expect(card_AS.value).toEqual(13);
 });
