220c1512da80f0f473d61b8e7e7f338bf67c7945
anti-matt-er <m.burrows@live.co.uk>
2021-01-05 00:51:00 +0000

    implement proper shuffling function, fisher-yates

--- src/services/game/Deck.js
+++ src/services/game/Deck.js
@@ -17,6 +17,13 @@ class Deck {
         });
     }
 
+    shuffle() {
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+        }
+    }
+
     bad_shuffle() {
         this.cards.sort(() => .5 - Math.random()); // BIASED, FOR TESTING
     }
