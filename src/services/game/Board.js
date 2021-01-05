66ad2a7d1548499349812e8d85ea7598238e0617
anti-matt-er <m.burrows@live.co.uk>
2021-01-05 18:39:36 +0000

    Implement errors for wrong card counts each street

--- src/services/game/Board.js
+++ src/services/game/Board.js
@@ -23,17 +23,28 @@ class Board {
     deal(cards) {
         switch (this.street) {
             case this.streets.preflop:
+                if (cards.length !== 3) {
+                    throw 'Error: Received invalid number of cards for Preflop, expected 3 got ' + cards.length;
+                }
                 this.cards.flop = cards;
                 this.street = this.streets.flop;
                 break;
             case this.streets.flop:
+                if (cards.length !== 1) {
+                    throw 'Error: Received invalid number of cards for Flop, expected 1 got ' + cards.length;
+                }
                 this.cards.turn = cards;
                 this.street = this.streets.turn;
                 break;
             case this.streets.turn:
+                if (cards.length !== 1) {
+                    throw 'Error: Received invalid number of cards for Turn, expected 1 got ' + cards.length;
+                }
                 this.cards.river = cards;
                 this.street = this.streets.river;
                 break;
+            case this.streets.river:
+                throw 'Error: Board is at River and can accept no more cards!';
         }
     }
 }
