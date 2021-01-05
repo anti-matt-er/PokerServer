71ac0a338d41b7697178a35fa4e417da5586e82a
anti-matt-er <m.burrows@live.co.uk>
2021-01-05 20:23:45 +0000

    assign hand to player

--- src/services/game/Player.js
+++ src/services/game/Player.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { Card } from './Card';
+
 class Player {
     constructor() {
         this.reset();
@@ -7,6 +9,22 @@ class Player {
 
     reset() {
         this.hand = [];
+    }
+
+    deal(cards) {
+        if (!Array.isArray(cards)) {
+            throw 'Error: `cards` must be an array!';
+        }
+        const invalid_hand_error = 'Error: `cards` must contain exactly 2 cards!';
+        if (cards.length !== 2) {
+            throw invalid_hand_error;
+        }
+        cards.forEach(card => {
+            if (!(card instanceof Card)) {
+                throw invalid_hand_error;
+            }
+        });
+        this.hand = cards;
     }
 }
 exports.Player = Player;
