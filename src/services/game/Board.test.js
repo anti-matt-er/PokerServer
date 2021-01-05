4b0e026e861850534f5babfe40be39007ef4c161
anti-matt-er <m.burrows@live.co.uk>
2021-01-05 18:04:30 +0000

    test board rejects wrong no. of cards per street

---
+++ src/services/game/Board.test.js
@@ -0,0 +1,56 @@
+'use strict';
+
+import { Board } from './Board';
+
+const board = new Board();
+
+it('should only accept the right amount of cards on each street', () => {
+    // PREFLOP
+    expect(() => {
+        board.reset();
+        board.deal([1, 2]);
+    }).toThrow('invalid number of cards');
+    expect(() => {
+        board.reset();
+        board.deal([1, 2, 3, 4]);
+    }).toThrow('invalid number of cards');
+    // FLOP
+    expect(() => {
+        board.reset();
+        board.deal([1, 2, 3]); // valid, progresses to flop
+        board.deal([]);
+    }).toThrow('invalid number of cards');
+    expect(() => {
+        board.reset();
+        board.deal([1, 2, 3]); // valid, progresses to flop
+        board.deal([1, 2]);
+    }).toThrow('invalid number of cards');
+    // TURN
+    expect(() => {
+        board.reset();
+        board.deal([1, 2, 3]); // valid, progresses to flop
+        board.deal([1]); // valid, progresses to turn
+        board.deal([]);
+    }).toThrow('invalid number of cards');
+    expect(() => {
+        board.reset();
+        board.deal([1, 2, 3]); // valid, progresses to flop
+        board.deal([1]); // valid, progresses to turn
+        board.deal([1, 2]);
+    }).toThrow('invalid number of cards');
+    // RIVER
+    expect(() => {
+        board.reset();
+        board.deal([1, 2, 3]); // valid, progresses to flop
+        board.deal([1]); // valid, progresses to turn
+        board.deal([1]); // valid, progresses to river
+        board.deal([]);
+    }).toThrow('invalid number of cards');
+    expect(() => {
+        board.reset();
+        board.deal([1, 2, 3]); // valid, progresses to flop
+        board.deal([1]); // valid, progresses to turn
+        board.deal([1]); // valid, progresses to river
+        board.deal([1, 2]);
+    }).toThrow('invalid number of cards');
+});
