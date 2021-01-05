6917bff3d89e4026d7a3da420e1d52ef164e8005
anti-matt-er <m.burrows@live.co.uk>
2021-01-05 18:47:27 +0000

    change card count test to use real cards

--- src/services/game/Board.test.js
+++ src/services/game/Board.test.js
@@ -1,49 +1,60 @@
 'use strict';
 
 import { Board } from './Board';
+import { Card } from './Card';
 
 const board = new Board();
 
 it('should only accept the right amount of cards on each street', () => {
+    const card_4D = new Card('4', 'D');
+    const card_TD = new Card('T', 'D');
+    const card_KH = new Card('K', 'H');
+    const card_AS = new Card('A', 'S');
+    const zero_cards = [];
+    const one_card = [card_4D];
+    const two_cards = [card_4D, card_TD];
+    const three_cards = [card_4D, card_TD, card_KH];
+    const four_cards = [card_4D, card_TD, card_KH, card_AS]
+
     // PREFLOP
     expect(() => {
         board.reset();
-        board.deal([1, 2]);
+        board.deal(two_cards);
     }).toThrow('invalid number of cards');
     expect(() => {
         board.reset();
-        board.deal([1, 2, 3, 4]);
+        board.deal(four_cards);
     }).toThrow('invalid number of cards');
     // FLOP
     expect(() => {
         board.reset();
-        board.deal([1, 2, 3]); // valid, progresses to flop
-        board.deal([]);
+        board.deal(three_cards); // valid, progresses to flop
+        board.deal(zero_cards);
     }).toThrow('invalid number of cards');
     expect(() => {
         board.reset();
-        board.deal([1, 2, 3]); // valid, progresses to flop
-        board.deal([1, 2]);
+        board.deal(three_cards); // valid, progresses to flop
+        board.deal(two_cards);
     }).toThrow('invalid number of cards');
     // TURN
     expect(() => {
         board.reset();
-        board.deal([1, 2, 3]); // valid, progresses to flop
-        board.deal([1]); // valid, progresses to turn
-        board.deal([]);
+        board.deal(three_cards); // valid, progresses to flop
+        board.deal(one_card); // valid, progresses to turn
+        board.deal(zero_cards);
     }).toThrow('invalid number of cards');
     expect(() => {
         board.reset();
-        board.deal([1, 2, 3]); // valid, progresses to flop
-        board.deal([1]); // valid, progresses to turn
-        board.deal([1, 2]);
+        board.deal(three_cards); // valid, progresses to flop
+        board.deal(one_card); // valid, progresses to turn
+        board.deal(two_cards);
     }).toThrow('invalid number of cards');
     // RIVER
     expect(() => {
         board.reset();
-        board.deal([1, 2, 3]); // valid, progresses to flop
-        board.deal([1]); // valid, progresses to turn
-        board.deal([1]); // valid, progresses to river
+        board.deal(three_cards); // valid, progresses to flop
+        board.deal(one_card); // valid, progresses to turn
+        board.deal(one_card); // valid, progresses to river
         board.deal(['anything']);
     }).toThrow('no more cards');
 });
