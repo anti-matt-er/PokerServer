11a4f2ff855275a1c167408b7abf9a077cb5b810
anti-matt-er <m.burrows@live.co.uk>
2021-01-05 17:22:42 +0000

    implement deck deal function

--- src/services/game/Deck.js
+++ src/services/game/Deck.js
@@ -2,6 +2,8 @@
 
 import { Definitions } from './Card_Definitions';
 import { Card } from './Card';
+import { Board } from './Board';
+import { Player } from './Player';
 
 class Deck {
     constructor() {
@@ -29,11 +31,34 @@ class Deck {
         this.cards.sort(() => .5 - Math.random()); // BIASED, FOR TESTING
     }
 
-    deal(player) {
-        player.hand = [
-            this.cards.pop().card,
-            this.cards.pop().card
-        ];
+    deal_cards(count) {
+        let cards = [];
+        for (let i = 0; i < count; i++) {
+            cards.push(this.cards.pop().card);
+        }
+        return cards;
+    }
+
+    deal(to) {
+        if (to instanceof Player) {
+            to.hand = this.deal_cards(2);
+        }
+        else if (to instanceof Board) {
+            switch (to.street) {
+                case to.streets.preflop:
+                    to.deal(this.deal_cards(3));
+                    break;
+                case to.streets.flop:
+                    to.deal(this.deal_cards(1));
+                    break;
+                case to.streets.turn:
+                    to.deal(this.deal_cards(1));
+                    break;
+            }
+        }
+        else {
+            throw 'Error: Provided object is not instance of Player or Board!';
+        }
     }
 }
 exports.Deck = Deck;
