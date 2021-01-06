034d8a6e23e85de074c01fbd3e0ee8db904e84a0
anti-matt-er <m.burrows@live.co.uk>
2021-01-06 03:43:23 +0000

    implement card sort

--- src/services/game/Judge.js
+++ src/services/game/Judge.js
@@ -3,10 +3,18 @@
 import { Rankings, Weights } from './Hand_Rankings';
 import { Definitions } from './Card_Definitions';
 import { Card } from './Card';
+import { array } from 'joi';
 
 class Judge {
-    static assess() {
-        return {ranking: ''};
+    static assess(hand) {
+        let cards = this.sort(hand);
+        return { ranking: '' };
+    }
+
+    static sort(cards) {
+        return cards.sort((a, b) => {
+            return Definitions.ranks[a.rank].value - Definitions.ranks[b.rank].value;
+        });
     }
 }
 exports.Judge = Judge;
