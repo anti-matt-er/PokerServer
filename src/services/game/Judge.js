f73b1453971aec5231b31b5e393017322d7f4faa
anti-matt-er <m.burrows@live.co.uk>
2021-01-06 20:08:46 +0000

    identify flush

--- src/services/game/Judge.js
+++ src/services/game/Judge.js
@@ -7,14 +7,40 @@ import { array } from 'joi';
 
 class Judge {
     static assess(hand) {
-        let cards = this.sort(hand);
-        return { ranking: '' };
+        let ranking = '';
+        let cards = hand.player_hand.concat(hand.board);
+        let flush = this.get_flush(cards);
+        if (flush) {
+            ranking = 'FL';
+        }
+        return { ranking: ranking };
     }
 
     static sort(cards) {
         return cards.sort((a, b) => {
-            return Definitions.ranks[a.rank].value - Definitions.ranks[b.rank].value;
+            return (
+                Definitions.ranks[a.rank].value -
+                Definitions.ranks[b.rank].value
+            );
         });
+    }
+
+    static get_flush(cards) {
+        let suit_count = {};
+        let flush_suit = false;
+        cards.some((card) => {
+            if (card.suit in suit_count) {
+                suit_count[card.suit] += 1;
+            }
+            else {
+                suit_count[card.suit] = 1;
+            }
+            if (suit_count[card.suit] >= 5) {
+                flush_suit = card.suit;
+                return;
+            }
+        });
+        return flush_suit;
     }
 }
 exports.Judge = Judge;
