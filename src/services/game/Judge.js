f71c43d60becfb1c022aa6d5c816249ef1515b90
anti-matt-er <m.burrows@live.co.uk>
2021-01-07 01:29:46 +0000

    identify straight

--- src/services/game/Judge.js
+++ src/services/game/Judge.js
@@ -9,9 +9,21 @@ class Judge {
     static assess(hand) {
         let ranking = '';
         let cards = hand.player_hand.concat(hand.board);
+        cards = this.sort(cards);
         let flush = this.get_flush(cards);
+        let straight = this.get_straight(cards);
+        let ace_low_straight = false;
+        if (!straight) {
+            ace_low_straight = this.get_straight(cards, true);
+            straight = ace_low_straight;
+        }
         if (flush) {
             ranking = 'FL';
+            if (straight) {
+                ranking = 'SF';
+            }
+        } else if (straight) {
+            ranking = 'ST';
         }
         return { ranking: ranking };
     }
@@ -28,19 +40,46 @@ class Judge {
     static get_flush(cards) {
         let suit_count = {};
         let flush_suit = false;
-        cards.some((card) => {
+        for (let card of cards) {
             if (card.suit in suit_count) {
                 suit_count[card.suit] += 1;
-            }
-            else {
+            } else {
                 suit_count[card.suit] = 1;
             }
             if (suit_count[card.suit] >= 5) {
                 flush_suit = card.suit;
-                return;
+                break;
             }
-        });
+        }
         return flush_suit;
+    }
+
+    static get_straight(cards, ace_low = false) {
+        let straight_run = 1;
+        let last_value = null;
+        if (ace_low) {
+            let aces = cards.filter((card) => card.rank === 'A');
+            let rest = cards.filter((card) => card.rank !== 'A');
+            cards = aces.concat(rest);
+        }
+        for (let card of cards) {
+            let value = card.value;
+            if (ace_low && card.rank === 'A') {
+                value = 0;
+            }
+            if (last_value !== null) {
+                if (value === last_value + 1) {
+                    straight_run += 1;
+                    if (straight_run >= 5) {
+                        break;
+                    }
+                } else if (value !== last_value) {
+                    straight_run = 1;
+                }
+            }
+            last_value = value;
+        }
+        return straight_run >= 5;
     }
 }
 exports.Judge = Judge;
