29e87cbe6f1b50539dd876f097df6a69bf8dcac1
anti-matt-er <m.burrows@live.co.uk>
2021-01-11 21:25:05 +0000

    use game object in tests

--- src/services/game/Player.test.js
+++ src/services/game/Player.test.js
@@ -2,21 +2,23 @@
 
 import { Player } from './Player';
 import { Card } from './Card';
+import { Game } from './Game';
 
 describe('hand', () => {
     const card_KH = new Card('KH');
     const card_AS = new Card('AS');
     const card_TD = new Card('TD');
+    const game = new Game();
 
     it('should reset correctly', () => {
-        const player = new Player();
+        const player = new Player(game);
         player.hand = [card_KH, card_AS];
         player.reset();
         expect(player.hand).toEqual([]);
     });
 
     it('should accept exactly 2 dealt cards', () => {
-        const player = new Player();
+        const player = new Player(game);
 
         expect(() => {
             player.deal('not an array');
@@ -49,7 +51,7 @@ describe('hand', () => {
     });
 
     it('should not accept more cards if there\'s already a hand', () => {
-        const player = new Player();
+        const player = new Player(game);
 
         expect(() => {
             player.deal([card_KH, card_AS]);
@@ -65,3 +67,22 @@ describe('hand', () => {
     });
 });
 
+describe('game', () => {
+    it('should fail if no Game object is provided', () => {
+        expect(() => {
+            const player = new Player();
+        }).toThrow('must provide a Game object');
+    
+        expect(() => {
+            const player = new Player('not a Game object');
+        }).toThrow('must provide a Game object');
+
+        expect(() => {
+            const player = new Player(123);
+        }).toThrow('must provide a Game object');
+
+        expect(() => {
+            const player = new Player({});
+        }).toThrow('must provide a Game object');
+    });
+});
