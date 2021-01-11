0609a5bae948f585ddc82b126e3cf8d90b77be61
anti-matt-er <m.burrows@live.co.uk>
2021-01-11 23:20:05 +0000

    implement some states

--- src/services/game/Player.js
+++ src/services/game/Player.js
@@ -10,13 +10,25 @@ class Player extends StateMachine {
             throw 'Error: A Game object must be provided';
         }
         super({
-            init: 'Seating'
+            init: 'Seating',
+            transitions: [
+                { name: 'seat', from: 'Seating', to: 'Idle' },
+                { name: 'action', from: 'Idle', to: 'Action' },
+                { name: 'act', from: 'Action', to: 'Idle' },
+                { name: 'allin', from: ['Idle', 'Action'], to: 'All-in' },
+                { name: 'reset', from: '*', to: 'Seating' }
+            ],
+            methods: {
+                onReset: () => {
+                    this.hand = [];
+                    this.bet = 0;
+                    this.bb = false;
+                }
+            }
         });
+        this.game = game;
+        this.chips = 0;
         this.reset();
-    }
-
-    reset() {
-        this.hand = [];
     }
 
     deal(cards) {
@@ -37,6 +49,53 @@ class Player extends StateMachine {
             }
         });
         this.hand = cards;
+    }
+
+    charge(amount) {
+        this.bet += Math.min(amount, this.chips);
+        this.chips -= amount;
+        
+        if (this.chips <= 0) {
+            this.chips = 0;
+            this.allin();
+        }
+    }
+
+    charge_ante() {
+        this.charge(this.game.ante);
+    }
+
+    charge_small_blind() {
+        this.charge(this.game.small_blind);
+    }
+
+    charge_big_blind() {
+        this.charge(this.game.big_blind);
+        this.bb = true;
+    }
+
+    call() {
+        this.act();
+    }
+
+    raise() {
+        this.act();
+    }
+
+    check() {
+        if (this.bet === 0) {
+            this.act();
+        }
+    }
+
+    fold() {
+        if (!this.bb) {
+            this.act();
+        }
+    }
+
+    quit() {
+        
     }
 }
 exports.Player = Player;
