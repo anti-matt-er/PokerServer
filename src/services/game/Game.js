5c0fc4bccf57ace3184236970a637e7cc9a810f6
anti-matt-er <m.burrows@live.co.uk>
2021-01-13 22:47:27 +0000

    implement state machine for game

--- src/services/game/Game.js
+++ src/services/game/Game.js
@@ -1,6 +1,12 @@
 'use strict';
 
-class Game {
+const StateMachine = require('javascript-state-machine');
 
+class Game extends StateMachine {
+    constructor() {
+        super({
+            init: 'Seating'
+        });
+    }
 }
 exports.Game = Game;
